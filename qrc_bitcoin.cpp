f82634e5c50a0ec8bf5d061ef8aeb13a56ecf37e
Peter Tran <obsidian@love.com>
2014-01-04 00:02:02 -0500

    removed personal build files

--- qrc_bitcoin.cpp
+++ qrc_bitcoin.cpp
@@ -1,7 +1,7 @@
 /****************************************************************************
 ** Resource object code
 **
-** Created: Fri Jan 3 23:35:23 2014
+** Created: Fri Jan 3 23:57:04 2014
 **      by: The Resource Compiler for Qt version 4.8.2
 **
 ** WARNING! All changes made in this file will be lost!
